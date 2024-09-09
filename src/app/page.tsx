f1b972488ef2a59f570a8f1c4dc71f1acb8ee259
Miguel Costa <miguel.costa.pascual@ethisys.co.uk>
2024-09-09 17:48:42 +0100

    use auth homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,39 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "../server/db/index"
 import Image from 'next/image'
 
 export const dynamic = "force-dynamic";
 
+async function Images() {
 
-export default async function HomePage() {
   const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4" >
+    {[...images, ...images, ...images].map((image, index) =>(
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <Image src={image.url} width={200} height={200} alt="image" />
+          <div>{image.name}</div>
+        </div> 
+      ))}
+  </div>
+  )
+}
+
+export default async function HomePage() {
+  
+
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4" >
-        {[...images, ...images, ...images].map((image, index) =>(
-            <div key={image.id + "-" + index} className="flex w-48 flex-col">
-              <Image src={image.url} width={200} height={200} alt="image" />
-              <div>{image.name}</div>
-            </div> 
-          ))}
-      </div>
+
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
